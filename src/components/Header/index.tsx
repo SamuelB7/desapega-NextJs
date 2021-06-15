049a094b162a6ba0f5fe3c68bcd8270bee1146cd
Samuel Belo <belo.samuel@gmail.com>
2021-06-15 20:43:57 -0300

    initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+    return (
+        <header>
+            <img src="#" alt="Logomarca" />
+
+            <div>
+                <input type="text"></input>
+            </div> 
+        </header>
+    )
+}
